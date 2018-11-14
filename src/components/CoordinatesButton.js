ec5c1134d433f328d481d50b9eca0fd3d5c99ab0
Katrina Pritchard <pritckat@gmail.com>
2018-11-14 19:09:16 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 class CoordinateButton extends React.Component {
-  
+
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click Me!</button>
+    )
+  }
 }
 
 export default CoordinateButton
